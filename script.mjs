435f5783dd85fea27d2b5d819b0638765a2e4a34
maigre <thomas@37m.gr>
2025-03-12 18:25:32 +0100

    prevent re-force

--- script.mjs
+++ script.mjs
@@ -14,106 +14,128 @@ import { execSync } from 'child_process';
 import { exit } from 'process';
 import readline from 'readline';
 
-var CONF= {
-    'path': null,
-    'pattern': '_LED_',
-    'force': false,
-    'output': [1920,1080],
-    'screens': {
-        '256': [256, 256, 0.5],
-        '512': [512, 512, 0.5],
-    },
-    'retry': 15
+var CONF;
+
+// CONFIG Load 
+//
+function loadConfig() 
+{
+    CONF = {}
+    if (fs.existsSync('config.json')) {
+        const config = JSON.parse(fs.readFileSync('config.json'));
+        CONF = {...CONF, ...config};
+    }
+    else {
+        console.log('No config file found..');
+        exit(1);
+    }
+
+    // Precompute scale ratios
+    for(const key in CONF.screens) {
+        
+        if (!CONF.screens[key]['resolution']) {
+            console.error('You must set the "resolution" for each screen in config.json');
+            process.exit(1);
+        }
+        if (!CONF.screens[key]['search']) {
+            console.error('You must set the "search" string for each screen in config.json');
+            process.exit(1);
+        }
+        if (!CONF.screens[key]['target']) {
+            console.error('You must set the "target" name for each screen in config.json');
+            process.exit(1);
+        }
+        if (!CONF.screens[key]['v_scale']) CONF.screens[key]['v_scale'] = 1;
+        if (!CONF.screens[key]['h_scale']) CONF.screens[key]['h_scale'] = 1;
+        if (!CONF.screens[key]['player']) CONF.screens[key]['player'] = CONF.screens[key]['resolution']
+        if (!CONF.screens[key]['align']) CONF.screens[key]['align'] = "center";
+        if (!CONF.screens[key]['force']) CONF.screens[key]['force'] = false;
+
+        // Crop Ratio
+        CONF.screens[key]['cropratio'] = CONF.screens[key]['resolution'][0] / CONF.screens[key]['resolution'][1]; 
+    }
+
+    if (!CONF.path || !fs.existsSync(CONF.path)) {
+    console.error('You must set a valid "path" in config.json');
+    process.exit(1);
+    }
 }
+loadConfig()
 
-// load 
-if (fs.existsSync('config.json')) {
-    const config = JSON.parse(fs.readFileSync('config.json'));
-    CONF = {...CONF, ...config};
-}
-else {
-    console.log('No config file found..');
-    exit(1);
-}
+// Find FFMPEG
+//
+var ffmpeg = 'ffmpeg';
 
-// Precompute scale ratios
-Object.keys(CONF.screens).forEach((key) => {
-    if (CONF.screens.length < 3) CONF.screens[key].push(1); // 1:1
-    CONF.screens[key][3] = CONF.screens[key][0]*CONF.screens[key][2] / CONF.screens[key][1]; // Crop Ratio
-});
-
-
-if (!CONF.path) {
-  console.error('You must set the folder path in config.json');
-  process.exit(1);
-}
-
-function processFile(filePath) {
+// Convert file
+//
+function processFile(filePath) 
+{
+    // valid
     if (!filePath) return;
-    
     const filename = path.basename(filePath);
     const folder = path.dirname(filePath);
     if (!fs.existsSync(filePath)) return;
 
-    // Ignore files that do not contain CONF.searchfor
-    if (!filename.includes(CONF.searchfor)) return;
-
-    // Ignore files that already have a LED resolution
-    for (const key of Object.keys(CONF.screens))
-        if (filename.includes(`_LED${key}_`)) return;
-
-    // Rescale
-    Object.keys(CONF.screens).forEach((key) => {
-        const outputFilename = filename.replace('_LED_', `_LED${key}_`);
-        const outputPath = path.join(folder, outputFilename);
+    // process each screen
+    for (const key in CONF.screens) 
+    {
+        const screen = CONF.screens[key];
+        if (!filename.includes(screen.search)) continue;
 
         // Skip if target file already exists
-        if (fs.existsSync(outputPath) && !CONF.force) return;  
+        const outputFilename = filename.replace(screen.search, screen.target);
+        const outputPath = path.join(folder, outputFilename);
+        if (fs.existsSync(outputPath) && !CONF.force && !screen.force) continue;  
 
-        console.log(`Converting ${filePath} to ${outputPath}`);
-        const scale = CONF.screens[key];
-
-        var crop = `crop=in_h*${scale[3]}:in_h:(in_w-in_h*${scale[3]})/2:0`;
-        if (scale[3] > 1) crop = `crop=in_w:in_w/${scale[3]}:0:(in_h-in_w/${scale[3]})/2`;
-
-        execSync(`ffmpeg -y -i "${filePath}" -vf "${crop},scale=${scale[0]}:${scale[1]},setsar=1/1,pad=${CONF.output[0]}:${CONF.output[1]}:0:0:black" "${outputPath}"`);
-        console.log(`Converted file ${outputFilename} in ${scale[0]}x${scale[1]} pixels`);
-    
-    });
+        console.log(`------\nConverting ${filePath} to ${outputPath}\n`);
+        
+        // Crop Ratio inside original file
+        const ratio = screen.cropratio;
+        var crop = `\
+        w='if(gt(a,${ratio}),ih*${ratio},iw)':
+        h='if(gt(a,${ratio}),ih,iw/${ratio})':
+        x='(iw-min(iw,ih*${ratio}))/2':
+        y='(ih-min(ih,iw/${ratio}))/2'`
+        
+        // Scale to target resolution
+        const scale = screen.resolution[0]*CONF.screens[key]['h_scale'] + ':' + screen.resolution[1]*CONF.screens[key]['v_scale'];
+        
+        // Pad to output resolution
+        var pad;
+        if (screen.align == "origin") 
+            pad = `${screen.player[0]}:${screen.player[1]}:0:0:black`;
+        else if (screen.align == "center") 
+            pad = `${screen.player[0]}:${screen.player[1]}:(ow-iw)/2:(oh-ih)/2:black`;
+        
+        // Execute
+        execSync(`ffmpeg -y -i "${filePath}" -vf "crop=${crop},scale=${scale},setsar=1/1,pad=${pad}" -c:a aac -b:a 128k -ar 44100 -c:v libx264 -profile:v main -level:v 4.1 -b:v 8M -maxrate 10M -bufsize 12M -tune fastdecode -g 50 -keyint_min 25 -metadata:s:v:0 "pixel_aspect=1/1" -movflags +faststart -x264-params "no-scenecut=1:nal-hrd=cbr" -pix_fmt yuv420p "${outputPath}"`);
+        console.log(`Converted file ${outputFilename} in ${scale} pixels`);
+    }
 }
 
+// Recursive folder processing
+//
 function processFolder(folder) 
 {
-    
     fs.readdirSync(folder).forEach(file => {
         
-        // ignore hidden files
         const filePath = path.join(folder, file);
         
-        if (fs.lstatSync(filePath).isDirectory()) {
-            // if hidden folder, ignore
-            if (file.startsWith('.')) {
-                // console.log(`Ignoring hidden folder ${file}`);
-                return;
-            }
-            processFolder(filePath);
-        }
-        else {
-            // ignore hidden files
-            if (file.startsWith('.')) {
-                // console.log(`Ignoring hidden file ${file}`);
-                return;
-            }
-            processFile(filePath);
-        }
+        // ignore hidden / conflict files
+        if (file.startsWith('.')) return;
+        if (file.includes('sync-conflict')) return;
+
+        // process
+        if (fs.lstatSync(filePath).isDirectory()) processFolder(filePath);
+        else processFile(filePath);
     });
-    
-    
 }
 
 var isRunning = false;
 var scheduledRun = null;
 
+// Run (periodic)
+//
 function run() {
     if (scheduledRun) clearTimeout(scheduledRun);
     if (isRunning) {
@@ -121,6 +143,7 @@ function run() {
         return;
     }
     isRunning = true;
+    
     console.log(`------------------------`);
     console.log(" ▗▄▖ ▗▖ ▗▖▗▄▄▄▖▗▄▖  ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▖  ▗▖▗▄▄▄▖▗▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▖ ")
     console.log("▐▌ ▐▌▐▌ ▐▌  █ ▐▌ ▐▌▐▌   ▐▌ ▐▌▐▛▚▖▐▌▐▌  ▐▌▐▌   ▐▌ ▐▌ █  ▐▌   ▐▌ ▐▌")
@@ -129,6 +152,7 @@ function run() {
     console.log("                                                                 ")
     console.log("                                                                 ")
     console.log("RUN: ");
+    
     Object.keys(CONF).forEach((key) => {
         console.log(`${key.padEnd(10)}: ${JSON.stringify(CONF[key])}`);
     })
@@ -136,10 +160,38 @@ function run() {
     processFolder(CONF.path);
     
     console.log('\nDONE.\n');
-    console.log('Waiting for next run in '+CONF.retry+' minutes (press key to trigger manually)');
     isRunning = false;
-    scheduledRun = setTimeout(run, CONF.retry*60*1000);
+    
+    // disable forced run after first run
+    CONF.force = false;
+    for (const key in CONF.screens) CONF.screens[key].force = false;
+    
+    if (CONF.retry > 0) {
+        console.log('Waiting for next run in '+CONF.retry+' minutes (press key to trigger manually)');
+        scheduledRun = setTimeout(run, CONF.retry*60*1000);
+    }
 }
+
+// If force is set in config, prompt confirmation
+let force = CONF.force;
+for (const key in CONF.screens) force = force || CONF.screens[key].force;
+if (force) {
+    console.log('WARNING: Force mode is enabled in config.json');
+    console.log('\tIt will force conversion of all files, even if target file already exists.');
+    console.log('\tDo you confirm? (y/n)');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('', (answer) => {
+        rl.close();
+        if (answer != 'y') {
+            console.log('Change config.json to disable force mode.');
+            process.exit();
+        }
+    });
+}
+
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
@@ -153,6 +205,5 @@ process.stdin.on('keypress', (str, key) => {
     }
 })
 
-// initial processing
-run()
+run();
 
